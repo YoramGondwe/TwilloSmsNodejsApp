d838b0c86431e5ede4135555febe2d5b4c50896b
Yoram Gondwe <yoram@thezig.io>
2020-04-20 23:58:03 +0200

    added a readme

---
+++ README.md
@@ -0,0 +1,7 @@
+#Send SMS using Twillo API
+create a `.env` file in your root project directory and this are the values expected 
+`ACC_ID= ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ API_KEY= XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`
+ 
+ ### To run the Application 
+ In your command line run `npm start` or to run with nodemon run `npm run dev`
--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,19 @@ var router = express.Router();
 require('dotenv').config();
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  const {body:{name}}= req
+  const {data:{firstName,lastName,phoneNumber}}= req.body;
   const accountSid = process.env.ACC_ID;
   const authToken = process.env.API_KEY;
   const client = require('twilio')(accountSid, authToken);
 
   client.messages
       .create({
-        body: `${res.body.firstName} has booked and appointment from the store Please contact them on`,
+        body: `${firstName} ${lastName} has booked and appointment from the store Please contact them on ${phoneNumber}`,
         from: '+15103909380',
-        to: '+260961874478'
+        to: '+XXXXXXXXXXXXXX'
       })
       .then(message => console.log(message.sid));
 
-    console.log(name);
   res.render('index', { title: "done" });
 });
 
