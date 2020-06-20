302f59a3d8139c6f220715286d2c5fca9b084475
Yoram Gondwe <yoram@thezig.io>
2020-06-20 09:55:53 +0200

    move from and to numbers to Env

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 create a `.env` file in your root project directory and this are the values expected 
 `ACC_ID= ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
  API_KEY= XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`
+ FROM_NUMBER= +XXXXXXXXXXX
+ TO_NUMBER= +XXXXXXXXXXXX
  
  ### To run the Application 
  In your command line run `npm start` or to run with nodemon run `npm run dev`
--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,15 @@ router.post('/', function(req, res, next) {
   const {data:{firstName,lastName,phoneNumber}}= req.body;
   const accountSid = process.env.ACC_ID;
   const authToken = process.env.API_KEY;
+  const fromNumber = process.env.FROM_NUMBER
+  const toNumber = process.env.TO_NUMBER
   const client = require('twilio')(accountSid, authToken);
 
   client.messages
       .create({
         body: `${firstName} ${lastName} has booked and appointment from the store Please contact them on ${phoneNumber}`,
-        from: '+15103909380',
-        to: '+XXXXXXXXXXXXXX'
+        from: fromNumber,
+        to: toNumber
       })
       .then(message => console.log(message.sid));
 
